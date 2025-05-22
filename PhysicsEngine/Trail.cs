d10d4aefce7917332c22d3aadb9370d891e1011f
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-05-22 23:01:32 +0200

    Improve trail renderer

--- PhysicsEngine/Trail.cs
+++ PhysicsEngine/Trail.cs
@@ -10,6 +10,10 @@ public class Trail
     public Queue<Vector2> Points = new();
     public int Capacity;
 
+    private Vector2 lastPoint;
+    private Vector2 accumulator;
+    private int accCount = 4;
+
     public Trail(int capacity)
     {
         Capacity = capacity;
@@ -18,6 +22,24 @@ public class Trail
 
     public void Update(Vector2 point)
     {
+        Vector2 diff = point - lastPoint;
+        lastPoint = point;
+
+        accumulator += diff;
+
+        int countThresh = 8;
+        int rangeThresh = 4;
+
+        if (accCount < countThresh &&
+            accumulator.LengthSquared() < rangeThresh * rangeThresh)
+        {
+            accCount++;
+            return;
+        }
+        
+        accCount = default;
+        accumulator = default;
+
         if (Points.Count >= Capacity)
         {
             Points.Dequeue();
@@ -30,18 +52,32 @@ public class Trail
     {
         Color startColor = Color.Lerp(color, Color.Black, 1f);
 
-        int count = 0;
+        int start = Capacity - Points.Count;
+        int count = start;
         Vector2 prevPoint = default;
+        Vector2 dir = default;
+
         foreach (Vector2 point in Points)
         {
-            if (count != 0)
+            if (count != start)
             {
-                float age = count / (float) Capacity;
-                spriteBatch.DrawLine(prevPoint, point, Color.Lerp(startColor, color, age), age * thickness + 1);
+                dir = point - prevPoint;
+                DrawLine(spriteBatch, prevPoint, point, dir, count, startColor, color, thickness);
             }
 
             prevPoint = point;
             count++;
         }
+
+        DrawLine(spriteBatch, prevPoint, lastPoint, dir, count, startColor, color, thickness);
+    }
+
+    private void DrawLine(
+        SpriteBatch spriteBatch, Vector2 prevPoint, Vector2 point, Vector2 dir,
+        int count, Color startColor, Color color, float thickness)
+    {
+        float age = count / (float) Capacity;
+        float thick = (age + 0.1f) / 1.1f * thickness;
+        spriteBatch.DrawLine(prevPoint - dir, point, Color.Lerp(startColor, color, age), thick);
     }
 }
